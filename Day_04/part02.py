7dfd1c69111152c9823eddac0b5c49fecd53ca32
Whoami354 <fabian.gregory98@hotmail.de>
2023-09-16 18:56:38 +0200

    Tag 04 gemacht

--- Day_04/part02.py
+++ Day_04/part02.py
@@ -4,21 +4,21 @@ with open("input", "r") as file:
     input = file.read().split("\n")
 
 def check_actual_field(field):
-    field_form = field.split(":")[0]
+    required_field = field.split(":")[0]
     field_value = field.split(":")[1]
 
     eye_colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
-    if field_value.isdigit() and field_form != "pid" and field_form != "hgt":
+    if field_value.isdigit() and required_field != "pid" and required_field != "hgt":
         field_value = int(field_value)
 
-    if field_form == "byr":
+    if required_field == "byr":
         return 1920 <= field_value <= 2002
-    if field_form == "iyr":
+    if required_field == "iyr":
         return 2010 <= field_value <= 2020
-    if field_form == "eyr":
+    if required_field == "eyr":
         return 2020 <= field_value <= 2030
-    if field_form == "hgt":
+    if required_field == "hgt":
         if not field_value.endswith("cm") and not field_value.endswith("in"):
             return False
         else:
@@ -28,14 +28,14 @@ def check_actual_field(field):
                 return 150 <= val <= 193
             else:
                 return 59 <= val <= 76
-    if field_form == "hcl":
+    if required_field == "hcl":
         return len(field_value) == 7
-    if field_form == "ecl":
+    if required_field == "ecl":
         if len(field_value) > 3:
             return False
         else:
             return field_value in eye_colors
-    if field_form == "pid":
+    if required_field == "pid":
         return len(field_value) == 9
 
     return True
