13d62be61f1af793d141df8bef54aa9ddfa7d07f
Whoami354 <fabian.gregory98@hotmail.de>
2023-09-16 19:11:09 +0200

    Tag 03 gemacht

--- Day_03/part02.py
+++ Day_03/part02.py
@@ -3,13 +3,13 @@ with open("input", "r") as file:
     # Zum Beispiel: Daten lesen, schreiben oder verarbeiten
     input = file.read().split("\n")
 
-going_different_way = [(1, 1), (5, 1), (7, 1), (1, 2)]
+different_ways = [(1, 1), (5, 1), (7, 1), (1, 2)]
 product_trees = 282
 count_trees = 0
 
 len_row = len(input[0])
 
-for coor in going_different_way:
+for coordinates in different_ways:
     x, y = 0, 0
     count_trees = 0
 
@@ -18,8 +18,8 @@ for coor in going_different_way:
         if input[y][x % len_row] == '#':
             count_trees += 1
 
-        x += coor[0]
-        y += coor[1]
+        x += coordinates[0]
+        y += coordinates[1]
 
     product_trees *= count_trees
 
